72cc0e964744b44d07ecef353a906ec0b7520504
MoneshVenkul <mvvenkul@gmail.com>
2022-06-11 20:45:02 +0530

    added multiply function

--- 1.sum_two_numbers.js
+++ 1.sum_two_numbers.js
@@ -3,4 +3,10 @@ function myFunction(a, b) {
     return a + b;
  }
 
+ function multiplyFunction(a,b){
+    return a * b;
+ }
+
  console.log(myFunction(4,2));
+
+ console.log(multiplyFunction(3,3));
